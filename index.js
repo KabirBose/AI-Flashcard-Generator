d2d55413f8593b02a7b3d76ded9d7d2a1d7911cb
Kabir Bose <kabirbose04@gmail.com>
2024-04-24 19:27:32 -0400

    added pdf read (ocr) functionality

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const dotenv = require("dotenv");
+const PdfOcr = require("node-pdf-ocr");
 const input = require("prompt-sync")({ sigint: true });
 const fs = require("fs");
 
 dotenv.config();
+
+PdfOcr("pdfs/dummy.pdf")
+  .then((text) => console.log(text))
+  .catch((err) => console.error(err));
 
 /*
     1. Upload .pdf file
@@ -13,34 +18,34 @@ dotenv.config();
     5. Write a prompt for generating flashcards
 */
 
-// Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+// // Access your API key as an environment variable (see "Set up your API key" above)
+// const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
-// Converts local file information to a GoogleGenerativeAI.Part object.
-function fileToGenerativePart(path, mimeType) {
-  return {
-    inlineData: {
-      data: Buffer.from(fs.readFileSync(path)).toString("base64"),
-      mimeType,
-    },
-  };
-}
+// // Converts local file information to a GoogleGenerativeAI.Part object.
+// function fileToGenerativePart(path, mimeType) {
+//   return {
+//     inlineData: {
+//       data: Buffer.from(fs.readFileSync(path)).toString("base64"),
+//       mimeType,
+//     },
+//   };
+// }
 
-async function run() {
-  // For text-and-image input (multimodal), use the gemini-pro-vision model
-  const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
+// async function run() {
+//   // For text-and-image input (multimodal), use the gemini-pro-vision model
+//   const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
 
-  const prompt = "What's different between these pictures?";
+//   const prompt = "What's different between these pictures?";
 
-  const imageParts = [
-    fileToGenerativePart("image1.png", "image/png"),
-    fileToGenerativePart("image2.jpeg", "image/jpeg"),
-  ];
+//   const imageParts = [
+//     fileToGenerativePart("image1.png", "image/png"),
+//     fileToGenerativePart("image2.jpeg", "image/jpeg"),
+//   ];
 
-  const result = await model.generateContent([prompt, ...imageParts]);
-  const response = await result.response;
-  const text = response.text();
-  console.log(text);
-}
+//   const result = await model.generateContent([prompt, ...imageParts]);
+//   const response = await result.response;
+//   const text = response.text();
+//   console.log(text);
+// }
 
-run();
+// run();
