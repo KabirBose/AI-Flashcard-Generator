4c9e79d209897f02a986857323d2a51441238a99
Kabir Bose <kabirbose04@gmail.com>
2024-04-24 19:44:55 -0400

    added terminal text

--- index.js
+++ index.js
@@ -6,46 +6,29 @@ const fs = require("fs");
 
 dotenv.config();
 
+console.log(`
+\x1b[46m\x1b[1mFLASHCARD GENERATOR by Kabir Bose
+\x1b[45mPowered by Google Gemini
+\x1b[0m
+`);
+
+let pdfText;
+
 PdfOcr("pdfs/dummy.pdf")
-  .then((text) => console.log(text))
+  .then((text) => {
+    console.log(text);
+    pdfText = text;
+  })
   .catch((err) => console.error(err));
 
 /*
     1. Upload .pdf file
     2. Use OCR to read the file
     3. Store text from file
+
     4. Input that data into Gemini
     5. Write a prompt for generating flashcards
 */
 
 // // Access your API key as an environment variable (see "Set up your API key" above)
 // const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-
-// // Converts local file information to a GoogleGenerativeAI.Part object.
-// function fileToGenerativePart(path, mimeType) {
-//   return {
-//     inlineData: {
-//       data: Buffer.from(fs.readFileSync(path)).toString("base64"),
-//       mimeType,
-//     },
-//   };
-// }
-
-// async function run() {
-//   // For text-and-image input (multimodal), use the gemini-pro-vision model
-//   const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
-
-//   const prompt = "What's different between these pictures?";
-
-//   const imageParts = [
-//     fileToGenerativePart("image1.png", "image/png"),
-//     fileToGenerativePart("image2.jpeg", "image/jpeg"),
-//   ];
-
-//   const result = await model.generateContent([prompt, ...imageParts]);
-//   const response = await result.response;
-//   const text = response.text();
-//   console.log(text);
-// }
-
-// run();
