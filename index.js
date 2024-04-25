e33331da1fe3ed5fad07fd8939b07871d2bea93e
Kabir Bose <kabirbose04@gmail.com>
2024-04-24 20:32:23 -0400

    functionality completed

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const dotenv = require("dotenv");
 const PdfOcr = require("node-pdf-ocr");
-const input = require("prompt-sync")({ sigint: true });
-const fs = require("fs");
+const fs = require("node:fs");
+// const input = require("prompt-sync")({ sigint: true });
+
+/*
+    1. Upload .pdf file
+    2. Use OCR to read the file
+    3. Store text from file
+    4. Input that data into Gemini
+    5. Write a prompt for generating flashcards
+    6. Take the Gemini output and put it into a markdown file
+*/
 
 dotenv.config();
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
 console.log(`
 \x1b[46m\x1b[1mFLASHCARD GENERATOR by Kabir Bose
@@ -13,22 +24,29 @@ console.log(`
 `);
 
 let pdfText;
+const randomNum = Math.random() * 100000;
 
 PdfOcr("pdfs/dummy.pdf")
-  .then((text) => {
-    console.log(text);
-    pdfText = text;
+  .then(() => {
+    async function run() {
+      const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+      const prompt = `${pdfText}\n\nGiven this article: Generate flashcards for me. If it's a long article then generate enough flashcards so that it would cover all of the main, most important points. Keep the flashcards detailed and really focus on getting all the important stuff.`;
+
+      const result = await model.generateContent(prompt);
+      const response = await result.response;
+      const text = response.text();
+
+      fs.writeFile(`decks/deck-${randomNum}.md`, text, (err) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(
+            `File written successfully to: decks/deck-${randomNum}.md`
+          );
+        }
+      });
+    }
+    run();
   })
   .catch((err) => console.error(err));
-
-/*
-    1. Upload .pdf file
-    2. Use OCR to read the file
-    3. Store text from file
-
-    4. Input that data into Gemini
-    5. Write a prompt for generating flashcards
-*/
-
-// // Access your API key as an environment variable (see "Set up your API key" above)
-// const genAI = new GoogleGenerativeAI(process.env.API_KEY);
