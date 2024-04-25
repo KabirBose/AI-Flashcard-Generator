87e2beca80b06971ccfc9a1bb9e488e02d14bf65
Kabir Bose <kabirbose04@gmail.com>
2024-04-24 22:26:27 -0400

    complete

--- index.js
+++ index.js
@@ -2,39 +2,45 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const dotenv = require("dotenv");
 const PdfOcr = require("node-pdf-ocr");
 const fs = require("node:fs");
-// const input = require("prompt-sync")({ sigint: true });
-
-/*
-    1. Upload .pdf file
-    2. Use OCR to read the file
-    3. Store text from file
-    4. Input that data into Gemini
-    5. Write a prompt for generating flashcards
-
-    6. Take the Gemini output and put it into a markdown file
-*/
+const input = require("prompt-sync")({ sigint: true });
 
 dotenv.config();
 
-const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-
-console.log(`
-\x1b[46m\x1b[1mFLASHCARD GENERATOR by Kabir Bose
-\x1b[45mPowered by Google Gemini
-\x1b[0m
-`);
-
 let pdfText;
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const randomNum = Math.random() * 100000;
 
-PdfOcr("pdfs/dummy.pdf")
+console.log(`
+███████╗██╗░░░░░░█████╗░░██████╗██╗░░██╗░█████╗░░█████╗░██████╗░██████╗░
+██╔════╝██║░░░░░██╔══██╗██╔════╝██║░░██║██╔══██╗██╔══██╗██╔══██╗██╔══██╗
+█████╗░░██║░░░░░███████║╚█████╗░███████║██║░░╚═╝███████║██████╔╝██║░░██║
+██╔══╝░░██║░░░░░██╔══██║░╚═══██╗██╔══██║██║░░██╗██╔══██║██╔══██╗██║░░██║
+██║░░░░░███████╗██║░░██║██████╔╝██║░░██║╚█████╔╝██║░░██║██║░░██║██████╔╝
+╚═╝░░░░░╚══════╝╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░
+
+░██████╗░███████╗███╗░░██╗███████╗██████╗░░█████╗░████████╗░█████╗░██████╗░
+██╔════╝░██╔════╝████╗░██║██╔════╝██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗██╔══██╗
+██║░░██╗░█████╗░░██╔██╗██║█████╗░░██████╔╝███████║░░░██║░░░██║░░██║██████╔╝
+██║░░╚██╗██╔══╝░░██║╚████║██╔══╝░░██╔══██╗██╔══██║░░░██║░░░██║░░██║██╔══██╗
+╚██████╔╝███████╗██║░╚███║███████╗██║░░██║██║░░██║░░░██║░░░╚█████╔╝██║░░██║
+░╚═════╝░╚══════╝╚═╝░░╚══╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝
+by KABIR BOSE
+`);
+
+const filename = input(
+  "Enter the name of the file you want to generate flash cards for (eg. sample.pdf): "
+);
+
+console.log(`Loading ${filename} ...`);
+
+PdfOcr(`pdfs/${filename}`)
   .then((text) => {
     pdfText = text;
 
     async function run() {
       const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
-      const prompt = `${pdfText}\n\nGiven this article: Generate flashcards for me. If it's a long article then generate enough flashcards so that it would cover all of the main, most important points. Keep the flashcards detailed and really focus on getting all the important stuff.`;
+      const prompt = `${pdfText}\n\nGiven this article: Generate flashcards for me. If it's a long article then generate enough flashcards so that it would cover all of the main, most important points. Keep the flashcards detailed and really focus on getting all the important stuff. Keep the flashcards organized so that users can organize what's on the front and on the back. Format it and make it nice looking by using the markdown language.`;
 
       const result = await model.generateContent(prompt);
       const response = await result.response;
